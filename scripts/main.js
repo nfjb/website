304e1200bc5ad55a785ac014b6ba4aab7b9572fb
ieee802 <hallo123box@web.de>
2018-05-13 13:26:18 +0200

    f

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,19 @@
 // Wrap every letter in a span
-$('.ml12').each(function(){
-  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
+
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml12 .letter',
-    translateX: [40,0],
-    translateZ: 0,
+    targets: '.ml3 .letter',
     opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1200,
+    easing: "easeInOutQuad",
+    duration: 2250,
     delay: function(el, i) {
-      return 500 + 30 * i;
+      return 150 * (i+1)
     }
   }).add({
-    targets: '.ml12 .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: function(el, i) {
-      return 100 + 30 * i;
-    }
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
   });
